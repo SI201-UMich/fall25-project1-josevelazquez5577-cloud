4775eb1c567f86be2ddd4d8f8863ed63e6cb6b28
Jose Velazquez <josevelazquez5577@gmail.com>
2025-10-13 20:52:53 -0400

    converted numeric data

--- project1.py
+++ project1.py
@@ -1,11 +1,24 @@
 import csv
 
 def csv_to_dict_list(filepath):
-    with opsen(filepath, 'r', encoding = 'utf-8') as file:
+    with open(filepath, 'r', encoding = 'utf-8') as file:
         dict_reader = csv.DictReader(file)
         list_of_dicts = list(dict_reader)
     return list_of_dicts
 
-file_path = 'data/SampleSuperstore 2.csv'
-superstore_dict = csv_to_dict_list(file_path)
-print(superstore_dict)
+def clean_data(superstore_dict):
+    for data in superstore_dict: 
+        for k,v in data.items():
+            if isinstance(v,str):
+                v.strip()
+            if k =='Sales':
+                data['Sales']=float(v)
+            if k=='Profit':
+                data['Profit']=float(v)
+    return superstore_dict
+
+if __name__ == "__main__":
+    file_path = 'data/SampleSuperstore 2.csv'
+    superstore_dict = csv_to_dict_list(file_path)
+    superstore_data = clean_data(superstore_dict)
+    print(superstore_data)
