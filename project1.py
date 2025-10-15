b1b6b9a933d061ca2480ca475d9fbe11d7400d84
Jose Velazquez <josevelazquez5577@gmail.com>
2025-10-15 13:48:59 -0400

    Second calculation was added

--- project1.py
+++ project1.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 import csv
+from operator import itemgetter
 
-def csv_to_dict_list(filepath):
+def load_csv(filepath):
     with open(filepath, 'r', encoding = 'utf-8') as file:
         dict_reader = csv.DictReader(file)
         list_of_dicts = list(dict_reader)
@@ -17,7 +19,7 @@ def clean_data(superstore_dict):
                 data['Profit']=float(v)
     return superstore_dict
 
-def calc_region_profit(superstore_data):
+def calc_region_profitability(superstore_data):
     profit_list = [
         {'Region': 'Central', 'Total Sales': 0.0, 'Total Profit': 0.0, 'Profit Margin': 0.0}, 
         {'Region': 'East', 'Total Sales': 0.0, 'Total Profit': 0.0, 'Profit Margin': 0.0},
@@ -43,9 +45,56 @@ def calc_region_profit(superstore_data):
     profit_list[3]['Profit Margin'] = profit_list[3]['Total Profit']/profit_list[3]['Total Sales']*100
     fieldnames = ['Region','Total Sales','Total Profit','Profit Margin']
     result_file = 'result/result1.csv'
-    list_to_csv(profit_list, result_file, fieldnames)
+    write_csv(profit_list, result_file, fieldnames)
 
-def list_to_csv(dict_list, result_file, fieldnames):
+def calc_top_subcats_by_region(superstore_data):
+    sorted_average_sales = get_average_sales(superstore_data)
+    sorted_top_average_sales = get_top_average_sales(sorted_average_sales, k = 5)
+    fieldnames = ['Region', 'Sub-Category', 'Average Sales']
+    result_file = 'result/result2.csv'
+    write_csv(sorted_top_average_sales, result_file, fieldnames)
+
+def get_average_sales(superstore_data):
+    group_data = defaultdict(lambda: {'Total Sales': 0, 'Count': 0})
+
+    for row in superstore_data:
+        key = (row['Region'], row['Sub-Category'])
+        group_data[key]['Total Sales'] += row['Sales']
+        group_data[key]['Count'] += 1
+
+    average_sales = []
+    for (region, subcategory), values in group_data.items():
+        average = values['Total Sales']/values['Count']
+        average_sales.append({'Region': region, 'Sub-Category': subcategory, 'Average Sales': average})
+    
+    sorted_average_sales = sorted(average_sales, key = itemgetter('Region', 'Average Sales'), reverse = True)
+    return sorted_average_sales
+
+def get_top_average_sales(sorted_average_sales, k = 5):
+    top_average_sales = []
+    count_south = 0
+    count_east = 0
+    count_west = 0
+    count_central = 0
+    for row in sorted_average_sales:
+        if row['Region'] == 'South' and count_south < k:
+            count_south += 1
+            top_average_sales.append(row)
+        elif row['Region'] == 'East' and count_east < k:
+            count_east += 1
+            top_average_sales.append(row)
+        elif row['Region'] == 'West' and count_west < k:
+            count_west += 1
+            top_average_sales.append(row)
+        elif row['Region'] == 'Central' and count_central < k:
+            count_central += 1
+            top_average_sales.append(row)
+    sorted_top_average_sales = sorted(top_average_sales, key = itemgetter('Region', 'Average Sales'), reverse = True)
+    return sorted_top_average_sales
+
+
+
+def write_csv(dict_list, result_file, fieldnames):
     with open(result_file, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
@@ -53,7 +102,8 @@ def list_to_csv(dict_list, result_file, fieldnames):
 
 if __name__ == "__main__":
     file_path = 'data/SampleSuperstore 2.csv'
-    superstore_dict = csv_to_dict_list(file_path)
+    superstore_dict = load_csv(file_path)
     superstore_data = clean_data(superstore_dict)
-    calc_region_profit(superstore_data)
+    calc_region_profitability(superstore_data)
+    calc_top_subcats_by_region(superstore_data)
     print(superstore_data)
